da33739192128f62d1086c0709712529ec966883
Kashif Sami <tinkeringtechie@users.noreply.github.com>
2017-08-16 01:32:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-var port = process.env.port || 3000;
+port = process.env.port || 3000;
 
 var express = require('express');
 var app = new express();
 
-app.route('/receive')
-.get(function(req,res){
-  res.status(200).send("Placeholder for BoldChat trigger receiver.");
-});
-
 app.route('/')
 .get(function(req,res){
   res.status(200).send("You've reached Homepage.");
+});
+
+app.route('/receive')
+.get(function(req,res){
+  res.status(200).send("Placeholder for BoldChat trigger receiver.");
 });
 
 app.listen(port, function(){
