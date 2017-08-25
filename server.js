b66d166b4eb036d67a1a4b997b47c4167cd821d8
Kashif Sami <tinkeringtechie@users.noreply.github.com>
2017-08-25 11:02:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.get('/',function(req,res){
 app.post('/boldlogs',function(req,res){
   res.status(200).send("Got BoldChat logs!");
   console.log("Received Bold POST!");
-  //console.log(req.query);
   console.log(req.body);
   console.log("\nFresh? \n" + req.fresh);
   console.log(req.get('Content-Type'));
