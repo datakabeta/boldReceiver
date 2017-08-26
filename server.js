6b64788ebbe2efcce560ef05660eeb962b1e59f0
Kashif Sami <tinkeringtechie@users.noreply.github.com>
2017-08-26 23:10:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 var express = require('express');
 var app = new express();
-var bodyParser = require('body-parser');
-
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/',function(req,res){
   res.status(200).send("You've reached the Homepage.");
 });
-
-app.use(bodyParser.json());
 
 app.post('/boldlogs',function(req,res){
   res.status(200).send("Got BoldChat logs!");
