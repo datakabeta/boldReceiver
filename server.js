806d0da9dfd7b4b5babc7817520f35bcff04b579
Kashif Sami <tinkeringtechie@users.noreply.github.com>
2017-08-26 23:19:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ app.post('/boldlogs',function(req,res){
   console.log(req.body);
   console.log("\nFresh? \n" + req.fresh);
   console.log(req.get('Content-Type'));
-  res.json(req.body);
 });
 
 app.listen(app.get('port'), function() {
