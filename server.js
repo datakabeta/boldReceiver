4eca4b83afe2d02b20e72691bdac13c9a27b807d
Kashif Sami <tinkeringtechie@users.noreply.github.com>
2017-08-26 23:09:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 var express = require('express');
 var app = new express();
+var bodyParser = require('body-parser');
+
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/',function(req,res){
   res.status(200).send("You've reached the Homepage.");
-  console.log("Homepage reached!");
 });
+
+app.use(bodyParser.json());
 
 app.post('/boldlogs',function(req,res){
   res.status(200).send("Got BoldChat logs!");
   console.log("Received Bold POST!");
-  console.log(req.body.fresh);
+  console.log(req.body);
   console.log("\nFresh? \n" + req.fresh);
   console.log(req.get('Content-Type'));
-});
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
