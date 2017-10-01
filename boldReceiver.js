6068036c3f0521674a82cf5e7e65c501d0595c27
Tinkering Techie <tinkeringtechie@users.noreply.github.com>
2017-10-02 00:23:04 +0530

    None
    None

---
+++ boldReceiver.js
@@ -0,0 +1,24 @@
+function main() {
+  var express = require('express');
+  var app = new express();
+  var bodyParser = require('body-parser');
+
+  app.set('port', (process.env.PORT || 5000));
+  app.use(bodyParser.json()); // for parsing application/json
+  app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+  //Sample homepage code.
+  app.get('/',function(req,res){
+    res.status(200).send("You've reached the Homepage.");
+  });
+
+  //Receives BoldChat POST requests
+  app.post('/boldlogs',function(req,res){
+    console.log("Chat Details: \n");
+    console.log(req.body);
+  });
+
+  app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+  });
+};
