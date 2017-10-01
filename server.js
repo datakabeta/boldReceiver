c867193472ed3eb9800809de28b3138cc4f371f8
Tinkering Techie <tinkeringtechie@users.noreply.github.com>
2017-10-02 02:09:20 +0530

    sdf

--- server.js
+++ server.js
@@ -13,22 +13,23 @@ function main() {
     console.log('Node app is running on port', app.get('port'));
   });
 
-  console.log("Got here2");
 
-  //Sample homepage code.
-  app.get('/',function(req,res){
-    res.status(200).send("You've reached the Homepage.");
-  });
-
-  console.log("Got here3");
-
-  app.use(bodyParser.json()); // for parsing application/json
-  app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-  //Receives BoldChat POST requests
-  app.post('/boldlogs',function(req,res){
-    console.log("Chat Details: \n");
-    console.log(req.body);
+  // console.log("Got here2");
+  //
+  // //Sample homepage code.
+  // app.get('/',function(req,res){
+  //   res.status(200).send("You've reached the Homepage.");
+  // });
+  //
+  // console.log("Got here3");
+  //
+  // app.use(bodyParser.json()); // for parsing application/json
+  // app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+  //
+  // //Receives BoldChat POST requests
+  // app.post('/boldlogs',function(req,res){
+  //   console.log("Chat Details: \n");
+  //   console.log(req.body);
   });
 
 
