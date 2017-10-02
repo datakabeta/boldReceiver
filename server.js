337a4e42c6cf66286406a35fc10bd8ea913f0382
Tinkering Techie <tinkeringtechie@users.noreply.github.com>
2017-10-02 22:48:17 +0530

    zxc

--- server.js
+++ server.js
@@ -4,33 +4,30 @@ function main() {
   var bodyParser = require('body-parser');
 
   console.log("Got here0");
-
+  app.use(bodyParser.json()); // for parsing application/json
+  app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
   app.set('port', (process.env.PORT || 3000));
 
   console.log("Got here1");
 
+  //Create HTTP server and listen on default port
   app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
   });
 
+  console.log("Got here2");
 
-  // console.log("Got here2");
-  //
-  // //Sample homepage code.
-  // app.get('/',function(req,res){
-  //   res.status(200).send("You've reached the Homepage.");
-  // });
-  //
-  // console.log("Got here3");
-  //
-  // app.use(bodyParser.json()); // for parsing application/json
-  // app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-  //
-  // //Receives BoldChat POST requests
-  // app.post('/boldlogs',function(req,res){
-  //   console.log("Chat Details: \n");
-  //   console.log(req.body);
+  //Sample homepage code.
+  app.get('/',function(req,res){
+    res.status(200).send("You've reached the Homepage.");
   });
 
+  console.log("Got here3");
+
+  //Receives BoldChat POST requests
+  app.post('/boldlogs',function(req,res){
+    console.log("Chat Details: \n");
+    console.log(req.body);
+  });
 
 };
