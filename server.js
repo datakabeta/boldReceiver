a02c6c86e3ad2945f826d459d724557a32507780
Kashif Sami <tinkeringtechie@users.noreply.github.com>
2017-10-02 23:35:26 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // function main() {
   var express = require('express');
   var app = new express();
@@ -7,13 +6,13 @@
   console.log("Got here0");
   app.use(bodyParser.json()); // for parsing application/json
   app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-  app.set('port', (process.env.PORT || 3000));
+  //app.set('port', (process.env.PORT || 3000));
 
   console.log("Got here1");
 
   //Create HTTP server and listen on default port
   app.listen(app.get('port'), function() {
-    console.log('Node app is running on port ', app.get('port'));
+   console.log('Node app is running on port ', app.get('port'));
   });
 
   console.log("Got here2");
@@ -32,41 +31,3 @@
   });
 
 // };
-=======
-var express = require('express');
-var app = new express();
-var bodyParser = require('body-parser');
-
-
-app.set('port', (process.env.PORT || 5000));
-
-app.get('/',function(req,res){
-  res.status(200).send("You've reached the Homepage.");
-});
-
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-app.post('/boldlogs',function(req,res){
-  console.log("Received Bold POST!");
-  console.log("Body: \n");
-  console.log(req.body);
-  console.log("\nFresh? \n" + req.fresh);
-//  console.log(req.get('Content-Type'));
-  //res.json(req.body);
-
-  console.log("Moving on... \n");
-  AID = process.env.AID || 0;
-  SETTINGSID = process.env.APISETTINGSID || 0;
-  KEY = process.env.APIKEY || 0;
-
-  console.log("AID: "+AID);
-  console.log("KEY: "+KEY);
-  console.log("SETTINGSID: "+SETTINGSID);
-
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
->>>>>>> 6117a74112f9233320229abe395e1a45991191df
