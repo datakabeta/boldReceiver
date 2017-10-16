7145d008b620e10f4e07cf7114dea2f871a6865c
Kashif Sami <tinkeringtechie@users.noreply.github.com>
2017-10-16 17:27:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,18 +3,16 @@
   var app = new express();
   var bodyParser = require('body-parser');
 
-  console.log("Got here0");
   app.use(bodyParser.json()); // for parsing application/json
   app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
   app.set('port', (process.env.PORT || 3000));
-
-  console.log("Got here1");
 
   //Create HTTP server and listen on default port
   app.listen(app.get('port'), function() {
    console.log('Node app is running on port ', app.get('port'));
   });
 
+  //Handles Bold exports
   app.post('/boldlogs',function(req,res){
     console.log("Chat Log \n");
     console.log(req.body);
@@ -24,19 +22,5 @@
   //404 Catch all
   app.use(function(req,res){
     res.send("Random page!");
+    console.log("\nAnother 404");
   })
-
-  // console.log("Got here2");
-  //
-  // //Sample homepage code.
-  // app.get('/',function(req,res){
-  //   res.status(200).send("You've reached the Homepage.");
-  // });
-  //
-  // console.log("Got here3");
-  //
-  // //Receives BoldChat POST requests
-  // app.post('/boldlogs',function(req,res){
-  //   console.log("Chat Details: \n");
-  //   console.log(req.body);
-  // });
